bd045c331d0ea595c7ed78f699d0a0f3a4c3b684
yukuber <154669988+yukuber@users.noreply.github.com>
2024-03-11 13:05:11 +0300

    Update IPrinter.cs

--- IPrinter.cs
+++ IPrinter.cs
@@ -4,10 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task_3_4
+interface IPrinter
 {
-    interface IPrinter
-    {
-        void Print();
-    }
+    void Print();
 }
