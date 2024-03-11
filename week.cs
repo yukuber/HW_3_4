389ceb3a8e17bf59ea0be8fbfd01ede0063e8cb1
yukuber <154669988+yukuber@users.noreply.github.com>
2024-03-11 15:02:40 +0300

    Create week.cs

---
+++ week.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Weeks: IPrinter
+{
+  void IPrinter.Print()        
+  {
+    foreach (weeks i in Enum.GetValues(typeof(weeks)))
+    {
+      Console.WriteLine(i);
+    }      
+    Console.WriteLine();
+  }
+  
+  enum weeks {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+    }
+}
