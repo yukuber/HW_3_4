5dcf59daad6b678f928e43cb7459decea2998ed3
yukuber <154669988+yukuber@users.noreply.github.com>
2024-03-12 20:29:21 +0300

    3-4

--- classes/two_dim.cs
+++ classes/two_dim.cs
@@ -5,11 +5,11 @@ namespace HW_3_4.classes;
 sealed class two_dim<Type> : Parent<Type>
 {
 
-    private int[,] array;
+    private Type[,] array;
 
     public two_dim(Ivalue_giver<Type> value_giver, int high_d2, int len_d2, bool fill_rand = false) : base(value_giver, fill_rand)
     {
-        array = new int[high_d2,len_d2];
+        array = new Type[high_d2,len_d2];
         Create();
     }
 
